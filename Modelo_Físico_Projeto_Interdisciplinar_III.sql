f4ea8fed3efde5a9eaeb26b720aec3901799147a
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2024-06-14 13:47:58 -0300

    atualização do código

--- Modelo_Físico_Projeto_Interdisciplinar_III.sql
+++ Modelo_Físico_Projeto_Interdisciplinar_III.sql
@@ -2,9 +2,9 @@ CREATE DATABASE uber_logistico;
 
 USE uber_logistico;
 
-CREATE TABLE prestadores 
+CREATE TABLE prestadores  -- PRESTADORES
 (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT NOT NULL AUTO_INCREMENT, -- A função de "Auto_increment" é ao mesmo tempo "not null".
     nome VARCHAR(80) NOT NULL,
     cpf_cnpj CHAR(11) NOT NULL,
     email VARCHAR(80) NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE prestadores
     
 );
 
-CREATE TABLE clientes
+CREATE TABLE clientes -- CLIENTES
 (
 	id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(80) NOT NULL,
@@ -39,7 +39,7 @@ CREATE TABLE clientes
     
 );
 
-CREATE TABLE categoria
+CREATE TABLE categoria -- CATEGORIA
 (
 	id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(80) NOT NULL,
@@ -47,10 +47,51 @@ CREATE TABLE categoria
     
     PRIMARY KEY (id),
     
-    UNIQUE INDEX (nome),
+    UNIQUE INDEX (nome), -- A junção de ambos já faz tranforma em "Index" para buscas?
     INDEX (),
     
 );
+
+CREATE TABLE ordem_servico -- ORDER DE SERVIÇO -- Devo colocar pontuações?
+(
+	id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(80) NOT NULL,
+    descricao VARCHAR(250) NOT NULL,
+    data_de_inicio DATETIME NOT NULL,  -- Como fica as datas e horários comparado com
+    data_de_termino DATETIME NOT NULL, -- modelo conceitual e lógico?
+    status_os VARCHAR NOT NULL, -- Pode colocar "status" assim?
+
+    
+    PRIMARY KEY (id),
+    
+    UNIQUE INDEX (nome),
+    INDEX (),
+
+    FOREIGN KEY (clientes_id) REFERENCES clientes (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY () REFERENCES  () ON DELETE RESTRICT ON UPDATE RESTRICT,
+);
+
+CREATE TABLE forma_pagamento -- FORMA DE PAGAMENTO
+(
+	id INT NOT NULL AUTO_INCREMENT,
+    dinheiro FLOAT NULL,
+    credito FLOAT NULL, -- Esse tipo de váriavel está correta?
+    debito FLOAT NULL,  -- Quando "null" é permitido, devo colocar?
+    pix FLOAT NULL, 
+    
+
+    
+    PRIMARY KEY (id),
+    
+    UNIQUE INDEX (nome),
+
+    INDEX (),
+
+    FOREIGN KEY (ordem_servico_id) REFERENCES order_servico (id) ON DELETE RESTRICT ON UPDATE RESTRICT,
+    FOREIGN KEY () REFERENCES  () ON DELETE RESTRICT ON UPDATE RESTRICT,
+);
+
+
 
 
 
